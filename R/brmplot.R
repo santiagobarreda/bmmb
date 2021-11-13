ba33772fb6bf06d7d1fda1cdbecf1ac6210db8f4
Santiago Barreda <sbarreda@ucdavis.edu>
2021-11-13 10:16:32 -0800

    Update brmplot.R

--- R/brmplot.R
+++ R/brmplot.R
@@ -3,10 +3,11 @@
 #'
 #' Make plots of brm model parameter means and intervals.
 #'
-#' @param mat --.
-#' @param ylim --.
-#' @param xlim --.
+#' @param mat a dataframe or matrix representing the posterior summary of brms model parameters.
+#' @param ylim optionally override the plot y range.
+#' @param xlim optionally override the plot x range.
 #' @param horizontal --.
+#' @param add --.
 #' @param xs --.
 #' @param col --.
 #' @param labels --.
@@ -24,10 +25,7 @@
 #' @param xaxs --.
 #' @param line --.
 #' @param nudge --.
-#' @param name --.
-#' @param name --.
-#' @param name --.
-#' @param name --.
+#' @param ... --.
 #' @return --.
 #' @export
 #' @examples
@@ -41,8 +39,8 @@ brmplot = function (mat, ylim=NULL, xlim = NULL, horizontal = TRUE, add = FALSE,
                     pch=16, lwd=2,cex=1.5, las=NA,cex.axis=1,grid=TRUE,cex.lab=1,
                     robust = FALSE, yaxs="r",xaxs="r",line=TRUE, nudge =0,...){
 
-  n = nrow (mat)
-  if (n > 500 & colnames(mat)[1] !="Estimate") mat = posterior_summary (mat, robust=robust)
+  #n = nrow (mat)
+  #if (n > 500 & colnames(mat)[1] !="Estimate") mat = brms::posterior_summary (mat, robust=robust)
   n = nrow (mat)
 
   if (horizontal){
@@ -57,16 +55,16 @@ brmplot = function (mat, ylim=NULL, xlim = NULL, horizontal = TRUE, add = FALSE,
       plot (0, type='n', ylim = ylim,xlim=xlim, xlab=xlab,xaxt='n',ylab = ylab,
             cex.axis=cex.axis,yaxs=yaxs,xaxs=xaxs,cex.lab=cex.lab,...)
       if (grid) grid()
-      if (line) abline(h=0)
-      if (line) abline(v=0)
-      points (xs,mat[,1], col=col,pch=pch,cex=cex)
+      if (line) graphics::abline(h=0)
+      if (line) graphics::abline(v=0)
+      graphics::points (xs,mat[,1], col=col,pch=pch,cex=cex)
     }
-    if (add)  points (xs, mat[,1], col=col, pch=pch,cex=cex)
+    if (add)  graphics::points (xs, mat[,1], col=col, pch=pch,cex=cex)
     if (is.na(las))las=1
-    if (labels[1]!="") axis (side=1, at = xs, labels = labels, las=las, cex.axis=cex.axis)
+    if (labels[1]!="") graphics::axis (side=1, at = xs, labels = labels, las=las, cex.axis=cex.axis)
 
     if (length(col)==1) col = rep (col, length(xs))
-    for (i in 1:n) segments (xs[i],mat[i,3],xs[i],mat[i,4],lwd=lwd, col=col[i])
+    for (i in 1:n) graphics::segments (xs[i],mat[i,3],xs[i],mat[i,4],lwd=lwd, col=col[i])
   }
   if (!horizontal){
     if (is.null(xs)) xs = (n:1)
@@ -80,16 +78,16 @@ brmplot = function (mat, ylim=NULL, xlim = NULL, horizontal = TRUE, add = FALSE,
       plot (0,type='n', ylim = ylim,xlim=xlim, ylab=ylab,yaxt='n',xlab = xlab,
             cex.axis=cex.axis,yaxs=yaxs,xaxs=xaxs,cex.lab=cex.lab,...)
       if (grid) grid()
-      if (line) abline(v=0)
-      points (mat[,1],xs, pch=pch, col=col,cex=cex)
+      if (line) graphics::abline(v=0)
+      graphics::points (mat[,1],xs, pch=pch, col=col,cex=cex)
 
     }
-    if (add) points (mat[,1],xs, pch=pch,cex=cex, col=col)
+    if (add) graphics::points (mat[,1],xs, pch=pch,cex=cex, col=col)
     if (is.na(las))las=2
-    if (labels[1]!="") axis (side=2, at = xs, labels = labels, las=las, cex.axis=cex.axis)
+    if (labels[1]!="") graphics::axis (side=2, at = xs, labels = labels, las=las, cex.axis=cex.axis)
 
     if (length(col)==1) col = rep (col, length(xs))
-    for (i in n:1) segments (mat[i,3],xs[i],mat[i,4],xs[i],lwd=lwd, col=col[i])
+    for (i in n:1) graphics::segments (mat[i,3],xs[i],mat[i,4],xs[i],lwd=lwd, col=col[i])
   }
 }
 
