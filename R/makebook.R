f36e05be9d44c52e839c382fe4ad5f274aee14a7
Santiago Barreda <sbarreda@ucdavis.edu>
2022-02-14 11:36:46 -0800

    Update makebook.R

--- R/makebook.R
+++ R/makebook.R
@@ -2,7 +2,8 @@
 #' Make word book
 #'
 #' --
-#'
+
+#' @param folder --.
 #' @export
 #' @examples
 #' \dontrun{
@@ -11,19 +12,19 @@
 #'
 #'
 
-makebook = function (){
+makebook = function (folder = "./"){
 
-  rmds = list.files ("./", pattern = ".Rmd")
+  rmds = list.files (folder, pattern = ".Rmd")
   n = length(rmds)
 
   for (i in 1:n){
     tmp = readLines (rmds[i])
     spots = which (tmp=="$$")
     if (length(spots)>0) tmp = tmp[-spots]
-    if (i==n) writeLines (tmp, 'book/index.Rmd')
-    if (i!=n) writeLines (tmp, paste0('book/0',i,'.Rmd'))
+    if (i==n) writeLines (tmp, '../book/index.Rmd')
+    if (i!=n) writeLines (tmp, paste0('../book/0',i,'.Rmd'))
   }
 
-  bookdown::render_book ('./book', "bookdown::word_document2", output_dir = ".")
+  bookdown::render_book ('../book', "bookdown::word_document2", output_dir = ".")
 
 }
