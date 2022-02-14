d47155fbb905156b624fbe112a29e48ea7597a81
Santiago Barreda <sbarreda@ucdavis.edu>
2022-02-14 11:36:48 -0800

    Update short_hypothesis.R

--- R/short_hypothesis.R
+++ R/short_hypothesis.R
@@ -1,16 +1,23 @@
 
 #' Short hypothesis printout
 #'
-#' Shorter brms hypothesis print statement. 
+#' Shorter brms hypothesis print statement.
 #'
-#' @param hypothesis brms hypothesis object.
+#' @param model brms hypothesis object.
+#' @param ... bla bla.
 #' @export
 #' @examples
 #' \dontrun{
 #'  # coming soon
 #' }
 
-short_hypothesis = function (hypothesis){
-  cbind (Hypothesis=hypothesis[[1]][,1], round (hypothesis[[1]][,2:5],2))
+short_hypothesis = function (model,...){
+  call = as.list( sys.call() )
+  tmp = do.call (brms::hypothesis, call[-1])
+  output = brms::posterior_summary  (tmp$samples)
+  output = data.frame (output)
+  attr(output,"hypothesis") = tmp[[1]][[1]]
+  attr(output,"samples") = tmp[[2]]
+  return (output)
 }
 
