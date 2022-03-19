4fa9844599f5ed8a451f5370efecc2a0512d3847
Santiago Barreda <sbarreda@ucdavis.edu>
2022-03-19 13:33:39 -0700

    Update short_hypothesis.R

--- R/short_hypothesis.R
+++ R/short_hypothesis.R
@@ -4,30 +4,29 @@
 #' Shorter brms hypothesis print statement.
 #'
 #' @param model brms hypothesis object.
-#' @param ... bla bla.
 #' @export
 #' @examples
 #' \dontrun{
 #'  # coming soon
 #' }
 
-short_hypothesis = function (model,...){
+short_hypothesis = function (model, ...){
   call = as.list( sys.call() )
   tmp = do.call (brms::hypothesis, call[-1])
-  output = list()
-  output[[1]] = brms::posterior_summary  (tmp$samples)
-  #output = data.frame (output)
-  output$hypotheses = tmp[[1]][[1]]
-  output$samples = tmp[[2]]
+  output = brms::posterior_summary  (tmp$samples)
+  output = data.frame (output)
+  attr(output,"hypothesis") = tmp[[1]][[1]]
+  attr(output,"samples") = tmp[[2]]
 
-  class(output) = "short_hypothesis"
+  #output = list()
+  #output[[1]] = brms::posterior_summary  (tmp$samples)
+  #output = data.frame (output)
+  #output$hypotheses = tmp[[1]][[1]]
+  #output$samples = tmp[[2]]
+
+  #class(output) = "short_hypothesis"
 
   return (output)
-}
-
-#' @export
-print.short_hypothesis = function (x, ...){
-  print (x[[1]])
 }
 
 
