e1af029c88d43ddcfe4fa4ffead0f32958301aec
Santiago Barreda <sbarreda@ucdavis.edu>
2022-03-21 08:31:42 -0700

    Create getcomments.R

---
+++ R/getcomments.R
@@ -0,0 +1,37 @@
+
+
+#' Get comments
+#'
+#' --
+
+#' @param file --.
+#' @param context --.
+#' @export
+#' @examples
+#' \dontrun{
+#' --
+#' }
+#'
+#'
+
+getcomments = function (file, context = TRUE){
+  text = readLines (file)
+  comments = grep("@@", text)
+  comments = text[comments]
+
+  if (!context){
+    for (i in 1:length (comments)){
+      spot = gregexpr ("@@", comments[i])[[1]]
+      tmp = strsplit (comments[i], "")[[1]]
+
+      open = which(tmp=="[")
+      close = which(tmp=="]")
+      open = open[which.min(open - spot)]
+      close = close[which.min(close - spot)]
+
+      comments[i] = paste0 (tmp[open:close], collapse="")
+    }
+  }
+  comments
+}
+
