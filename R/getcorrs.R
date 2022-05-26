07a59e551dbefe759c2b250d6ef8ddef49f4b6d1
Santiago Barreda <sbarreda@ucdavis.edu>
2022-05-26 14:39:42 -0700

    Update getcorrs.R

--- R/getcorrs.R
+++ R/getcorrs.R
@@ -27,47 +27,11 @@ getcorrs = function (model, factor){
                 c(cormat[,3,]),
                 c(cormat[,4,]))
   cormat = cormat[c(use),]
-  
+
   labs = names[c(use)]
   labs = gsub ("Intercept", "Int.",  labs)
   rownames (cormat) = labs
   colnames (cormat) = c("Estimate","Est.Error","Q2.5","Q97.5")
   cormat
-}
-
-
-#' Get random effect standard deviations
-#'
-#' --
-
-#' @param model --.
-#' @export
-#' @examples
-#' \dontrun{
-#' --
-#' }
-#'
-#'
-
-
-getsds = function (model){
-
-  components = brms::VarCorr(model)
-  output = data.frame()
-
-  for (i in 1:length(components)){
-    tmp = data.frame(components[[i]]$sd)
-    tmp$group = names(components)[i]
-    labs = rownames(tmp)
-    labs = gsub ("Intercept", paste0("Int.",names(components)[i]),  labs)
-    rownames(tmp) = labs
-    output = rbind(output, tmp)
-  }
-  fix = which(output$group=="residual__")
-  if (length (length(fix)>0)){
-    output$group[fix] = "sigma"
-    rownames(output)[fix] = "sigma"
-  }
-  output
 }
 
