81ebd0c743af52a5e1162d31e7067c426dbf9b33
Santiago Barreda <sbarreda@ucdavis.edu>
2022-05-26 14:39:40 -0700

    Update logits.R

--- R/logits.R
+++ R/logits.R
@@ -1,5 +1,5 @@
 
-#' Convert probability to logit
+#' Convert logit to probability
 #'
 #' --
 #'
@@ -15,7 +15,7 @@
 ztop = function (z) 1 / (1 + exp (-z) )
 
 
-#' Convert logit to probability
+#' Convert probability to logit
 #'
 #' --
 #'
@@ -29,6 +29,43 @@ ztop = function (z) 1 / (1 + exp (-z) )
 #'
 
 ptoz = function (p){
+  p[p==1] = .99  # if p=1, change to 0.99
+  p[p==0] = .01  # if p=0, change to 0.01 (i.e. 1-0.99)
+  log (p) - log(1-p)
+}
+
+
+
+#' Convert logit to probability
+#'
+#' --
+#'
+#' @param z --.
+#' @export
+#' @examples
+#' \dontrun{
+#' --
+#' }
+#'
+#'
+
+antilogit = function (z) 1 / (1 + exp (-z) )
+
+
+#' Convert probability to logit
+#'
+#' --
+#'
+#' @param p --.
+#' @export
+#' @examples
+#' \dontrun{
+#' --
+#' }
+#'
+#'
+
+logit = function (p){
   p[p==1] = .99  # if p=1, change to 0.99
   p[p==0] = .01  # if p=0, change to 0.01 (i.e. 1-0.99)
   log (p) - log(1-p)
