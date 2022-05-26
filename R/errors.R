e87dd5661b2be463a7c9c2ab43baa2ae4bc9e66d
Santiago Barreda <sbarreda@ucdavis.edu>
2022-05-26 14:39:47 -0700

    Update errors.R

--- R/errors.R
+++ R/errors.R
@@ -1,12 +1,37 @@
 
+#' Find root mean square
+#'
+#' --
+#' @param x --.
+#' @export
+#' @examples
+#' \dontrun{
+#' --
+#' }
+#'
+#'
 
 
 rms = function (x){
+  x = x[!is.na(x)]
   return (sqrt (mean(x^2)))
 }
 
 
+#' Find median absolute error
+#'
+#' --
+#' @param x --.
+#' @export
+#' @examples
+#' \dontrun{
+#' --
+#' }
+#'
+#'
+
 mae = function (x){
+  x = x[!is.na(x)]
   return (stats::median (abs(x)))
 }
 
