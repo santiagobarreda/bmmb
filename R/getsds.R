a563c0ead56dda9d5181454d2bac99f22ae4242b
Santiago Barreda <sbarreda@ucdavis.edu>
2022-05-26 14:39:37 -0700

    Create getsds.R

---
+++ R/getsds.R
@@ -0,0 +1,36 @@
+
+#' Get random effect standard deviations
+#'
+#' --
+
+#' @param model --.
+#' @export
+#' @examples
+#' \dontrun{
+#' --
+#' }
+#'
+#'
+
+
+getsds = function (model){
+
+  components = brms::VarCorr(model)
+  output = data.frame()
+
+  for (i in 1:length(components)){
+    tmp = data.frame(components[[i]]$sd)
+    tmp$group = names(components)[i]
+    labs = rownames(tmp)
+    labs = gsub ("Intercept", paste0("Int.",names(components)[i]),  labs)
+    rownames(tmp) = labs
+    output = rbind(output, tmp)
+  }
+  fix = which(output$group=="residual__")
+  if (length (length(fix)>0)){
+    output$group[fix] = "sigma"
+    rownames(output)[fix] = "sigma"
+  }
+  output
+}
+
