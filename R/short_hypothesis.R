7d6ef0ce155fb1919b275cc6f2566f1883acdf35
Santiago Barreda <sbarreda@ucdavis.edu>
2022-06-02 17:18:55 -0700

    Update short_hypothesis.R

--- R/short_hypothesis.R
+++ R/short_hypothesis.R
@@ -15,9 +15,11 @@ short_hypothesis = function (x, ...){
   tmp =brms::hypothesis (x, ...)
   output = brms::posterior_summary  (tmp$samples)
   output = data.frame (output)
-  attr(output,"hypothesis") = tmp[[1]]$Hypothesis
-  attr(output,"hypothesis") = tmp[[1]]$Hypothesis
-  if (!is.null(tmp[[1]]$Group)) attr(output,"group") = tmp[[1]]$Group
+  #attr(output,"hypothesis") = tmp[[1]]$Hypothesis
+  output$hypothesis = tmp[[1]]$Hypothesis
+
+  #if (!is.null(tmp[[1]]$Group)) attr(output,"group") = tmp[[1]]$Group
+  if (!is.null(tmp[[1]]$Group)) output$group = tmp[[1]]$Group
 
   attr(output,"samples") = tmp[[2]]
 
