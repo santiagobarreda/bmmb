37430bf83bb6a855e2fc054e1dfdc1debea5a1d2
Santiago Barreda <sbarreda@ucdavis.edu>
2022-06-06 14:56:23 -0700

    Update logits.R

--- R/logits.R
+++ R/logits.R
@@ -3,43 +3,7 @@
 #'
 #' --
 #'
-#' @param z --.
-#' @export
-#' @examples
-#' \dontrun{
-#' --
-#' }
-#'
-#'
-
-ztop = function (z) 1 / (1 + exp (-z) )
-
-
-#' Convert probability to logit
-#'
-#' --
-#'
-#' @param p --.
-#' @export
-#' @examples
-#' \dontrun{
-#' --
-#' }
-#'
-#'
-
-ptoz = function (p){
-  p[p==1] = .99  # if p=1, change to 0.99
-  p[p==0] = .01  # if p=0, change to 0.01 (i.e. 1-0.99)
-  log (p) - log(1-p)
-}
-
-
-
-#' Convert logit to probability
-#'
-#' --
-#'
+#' @rdname antilogit
 #' @param z --.
 #' @export
 #' @examples
@@ -52,10 +16,17 @@ ptoz = function (p){
 antilogit = function (z) 1 / (1 + exp (-z) )
 
 
+#' @rdname antilogit
+#' @rdname getsds
+#' @export
+ztop <- antilogit
+
+
 #' Convert probability to logit
 #'
 #' --
 #'
+#' @rdname logit
 #' @param p --.
 #' @export
 #' @examples
@@ -72,3 +43,6 @@ logit = function (p){
 }
 
 
+#' @rdname logit
+#' @export
+ptoz <- logit
