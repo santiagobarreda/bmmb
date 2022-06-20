7b9ac3e24eaf92da13f7289465803628ea228046
Santiago Barreda <sbarreda@ucdavis.edu>
2022-06-20 09:56:49 -0700

    Create getsamples.R

---
+++ R/getsamples.R
@@ -0,0 +1,24 @@
+
+
+#' Plot posterior samples in a data frame#'
+#' --
+#'
+#' @rdname getsamples
+#' @param model --.
+#' @export
+#' @examples
+#' \dontrun{
+#'  # coming soon
+#' }
+#'
+
+getsamples = function (model){
+  output = data.frame (brms::as_draws_matrix(model))
+  return (output)
+}
+
+
+#' @rdname getsamples
+#' @export
+get_samples <- getcorrs
+
