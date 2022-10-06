cc87cbde5bcd46c1043a7be47700b362d4abb51b
Santiago Barreda <sbarreda@ucdavis.edu>
2022-10-06 15:39:36 -0700

    Update plot_map.R

--- R/plot_map.R
+++ R/plot_map.R
@@ -4,6 +4,9 @@
 #'
 #' @param maps --.
 #' @param new --.
+#' @param colors --.
+#' @param xlim --.
+#' @param ylim --.
 #' @export
 #' @examples
 #' \dontrun{
@@ -12,15 +15,22 @@
 #'
 #'
 
+plot_map = function (maps, new = TRUE, colors = NULL, xlim = NULL, ylim = NULL){
 
-plot_map = function (maps, new = TRUE, colors = NULL){
-
-  if (new) plot (0,type='n', xlim=xlim,ylim=ylim)
-  if (all(is.null(colors))) colors = rep(15, adjustcolor (bmmb::cols[i], alpha.f=.2), 10)
-
-
-  for (i in 1:length(maps)){
-    polygon (maps[[i]], col = colors[i],lwd=3)
+  if (is.null(xlim)){
+    tmp = do.call(rbind,maps)
+    xlim = range(tmp[,1])
+  }
+  if (is.null(ylim)){
+    tmp = do.call(rbind,maps)
+    ylim = range(tmp[,2])
+  }
+  if (new)
+    plot(0, type = "n", xlim = xlim, ylim = ylim, xaxs='i',yaxs='i')
+  if (all(is.null(colors)))
+    colors = rep(15, grDevices::adjustcolor(bmmb::cols[i],
+                                            alpha.f = 0.2), 10)
+  for (i in 1:length(maps)) {
+    graphics::polygon(maps[[i]], col = colors[i], lwd = 3)
   }
 }
-
