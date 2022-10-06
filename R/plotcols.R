cda9d1382c7862b5e9a89145e82bdd6e5f7f4c5b
Santiago Barreda <sbarreda@ucdavis.edu>
2022-10-06 15:39:39 -0700

    Update plotcols.R

--- R/plotcols.R
+++ R/plotcols.R
@@ -16,7 +16,7 @@
 plotcols = function(cols = NA){
   if (is.na(cols[1])) plot (1:15,rep(1,15), col=bmmb::cols,pch=16,cex=9.5)
   if (!is.na(cols[1])) plot (1:15,rep(1,15), col=cols,pch=16,cex=12.5)
-  text (1:15,rep(1,15), 1:15, cex=2)
+  graphics::text (1:15,rep(1,15), 1:15, cex=2)
 }
 
 
